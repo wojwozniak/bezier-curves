114c25c806ac968836ac02968fd014fef9f616fb
Wojtek <pulchnymamut@gmail.com>
2023-12-18 00:59:38 +0100

    Update Center.tsx

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -3,11 +3,16 @@ import Editor from './Editor'
 import ButtonBar from './ButtonBar'
 import CurveList from './CurveList';
 
+const defaultMode: State = { label: "Dodaj punkt", dispatchTime: Date.now() };
+const defaultCurves: string[] = ["Główna krzywa", "Krzywa 2"];
+const defaultCurve: string = "Główna krzywa";
+const defaultCurveFromKeyboard: number = 1;
+
 const Center = () => {
-    const [activeMode, updateActiveMode] = useState<State>({ label: "Dodaj punkt", dispatchTime: Date.now() } as State);
-    const [curves, dispatchStateUpdate] = useState<string[]>(["Główna krzywa", "Krzywa 2"]);
-    const [currentCurve, setCurrentCurve] = useState<string>("Główna krzywa");
-    const [curveFromKeyboard, setCurveFromKeyboard] = useState<number>(1);
+    const [activeMode, updateActiveMode] = useState<State>(defaultMode);
+    const [curves, dispatchStateUpdate] = useState<string[]>(defaultCurves);
+    const [currentCurve, setCurrentCurve] = useState<string>(defaultCurve);
+    const [curveFromKeyboard, setCurveFromKeyboard] = useState<number>(defaultCurveFromKeyboard);
 
     useEffect(() => {
         if(curves.length >= curveFromKeyboard) {
