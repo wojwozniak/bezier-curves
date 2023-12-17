e4648cebfbde19343f8a9c4ddb7949bc02aa4613
Wojtek <pulchnymamut@gmail.com>
2023-12-18 00:50:09 +0100

    staticComponents

---
+++ src/staticComponents/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                Projekt na konkurs 2 z AN(L) - program do tworzenia czcionek
+            </p>
+            <p>
+                Autor: Wojciech Woźniak, grudzień 2023
+            </p>
+            <p>
+                Skróty klawiszowe: Q,W,E - dodaj, edytuj, usuń punkt; 1-9 - wybierz warstwę (jeśli istnieje)
+            </p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/staticComponents/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h3>Edytor krzywych Beziera</h3>
+        </div>
+    )
+}
+
+export default Header
