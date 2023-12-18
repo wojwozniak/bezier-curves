099960946cee70f88830b7eeb2a0d698babceb77
Wojtek <pulchnymamut@gmail.com>
2023-12-18 01:08:08 +0100

    Create drawPoints.ts

---
+++ src/functions/drawPoints.ts
@@ -0,0 +1,19 @@
+/**
+ * Funkcja rysująca punkty na kanwie
+ * @param {Coordinates[]} coordinates - współrzędne
+ * @param {CanvasRenderingContext2D} context - kontekst canvas
+ * @returns {void}
+ */
+export const drawPoints = (
+    coordinates: Coordinates[],
+    context: CanvasRenderingContext2D
+) => {
+    coordinates.forEach(({ x, y }, index) => {
+        context.beginPath();
+        context.arc(x, y, 5, 0, 2 * Math.PI);
+        context.fillStyle = index === coordinates.length - 1 ? '#FF0000' : '#000';
+        context.strokeStyle = index === coordinates.length - 1 ? '#FF0000' : '#000';
+        context.fill();
+        context.stroke();
+    });
+};
