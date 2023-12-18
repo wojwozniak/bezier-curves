acac564e332315d2c11f23251a20d266ce193b57
Wojtek <pulchnymamut@gmail.com>
2023-12-18 01:13:19 +0100

    Create findClosestPointIndex.ts

---
+++ src/functions/findClosestPointIndex.ts
@@ -0,0 +1,25 @@
+/**
+ * Funkcja szuka najbliższego punktu do podanych współrzędnych i zwraca jego indeks w tablicy
+ * @param {Coordinates[]} coords - współrzędne 
+ * @param {number} x - współrzędna x punktu do porównania
+ * @param {number} y - współrzędna y punktu do porównania 
+ * @returns {number} - indeks najbliższego punktu
+ */
+export const findClosestPointIndex = (
+    coords: Coordinates[],
+    x: number,
+    y: number
+) : number => {
+    let minDistance = Infinity;
+    let closestIndex = -1;
+
+    coords.forEach((point, index) => {
+        const distance = Math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2);
+        if (distance < 7 && distance < minDistance) {
+            minDistance = distance;
+            closestIndex = index;
+        }
+    });
+
+    return closestIndex;
+};
