27cd9740633fdecdce556e6f5b6debce5e45cb03
Wojtek <pulchnymamut@gmail.com>
2023-12-20 23:56:24 +0100

    wywalenie starych stylów

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './index.css';
-import './oldcss.css'
 import Header from './staticComponents/Header';
 import Footer from './staticComponents/Footer';
 import Center from './components/Center';
+import InstructionsModal from './components/InstructionsModal';
 
 const App: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-5 bg-bg">
       <Header />
+      <InstructionsModal />
       <Center />
       <Footer />
     </div>
--- src/staticComponents/Footer.tsx
+++ src/staticComponents/Footer.tsx
@@ -2,10 +2,7 @@ const Footer:React.FC = () => {
     return (
         <div className="mt-auto text-center text-gray-700">
             <p>
-                Projekt na konkurs 2 z AN(L) - program do tworzenia czcionek
-            </p>
-            <p>
-                Autor: Wojciech Woźniak, grudzień 2023
+                Edytor krzywych Béziera - Wojciech Woźniak, grudzień 2023
             </p>
         </div>
     )
